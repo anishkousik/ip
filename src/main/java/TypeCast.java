4dd9d51de735c366e00360bb2e7c438faff4b6cd
anishkousik <anishkousik@gmail.com>
2026-01-21 21:38:27 +0800

    Added task list functionality to the chatbot

--- src/main/java/TypeCast.java
+++ src/main/java/TypeCast.java
@@ -23,16 +23,29 @@ public class TypeCast {
         );
         
         Scanner scanner = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int numTasks = 0;
+        
         while (true) {
             
             String task = scanner.nextLine();
             
-            if (task.compareTo("bye") != 0) {
+            if (task.compareTo("bye") != 0 && task.compareTo("list") != 0) {
+                taskList[numTasks] = task;
+                numTasks++;
                 System.out.println("____________________________________________________________\r\n" + 
-                    task + "\n" + //
+                    "added: " + task + "\n" + //
                     "____________________________________________________________\r\n"
                 );
-            } else {
+            }   else if (task.compareTo("list") == 0) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < numTasks; i++) {
+                    System.out.println((i + 1) + ". " + taskList[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } 
+            
+            else {
                 System.out.println("""
                                    ____________________________________________________________\r
                                     Bye. Hope to see you again soon!\r
