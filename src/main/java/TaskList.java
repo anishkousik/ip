bec62f3b02eb9c42f2e0e4f130e489fbaffb3bb1
anishkousik <anishkousik@gmail.com>
2026-01-30 15:34:11 +0800

    Level A-MoreOOP: Add more OOP features

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+
+    public Task delete(int index) throws TypeCastException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new TypeCastException("Task index out of range.");
+        }
+        return tasks.remove(index);
+    }
+
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public void markTaskDone(int index) {
+        tasks.get(index).markDone();
+    }
+
+
+    public void markTaskNotDone(int index) {
+        tasks.get(index).markNotDone();
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
